daa6b6415f5d828c57c715c502fe7930fc459bfc
Ekaji <ekajionah@gmail.com>
2023-04-21 12:14:33 +0100

    added better error handling to getAllMessages

--- controller/messagesController.js
+++ controller/messagesController.js
@@ -17,6 +17,10 @@ module.exports.addMessage = async (req, res, next) => {
         if (! data) 
             return res.status(400).json({message: "failed to add message"})
 
+
+        
+
+
         return res.status(201).json({message: "added message"})
 
     } catch (err) {
@@ -50,31 +54,31 @@ module.exports.addMessage = async (req, res, next) => {
 // }
 
 module.exports.getAllMessages = async (req, res, next) => {
-  try {
-      const { from, to } = req.body;
-      if (!from || !to) {
-          return res.status(400).json({ message: 'Missing from/to parameter(s)' });
-      }
-      const messages = await messageModel.find({
-          users: {
-              $all: [from, to]
-          }
-      }).sort({ updatedAt: 1 }).maxTimeMS(30000);
+    try {
+        const {from, to} = req.body
+        if (!from || !to) {
+            return res.status(400).json({message: 'Missing from/to parameter(s)'});
+        }
+        const messages = await messageModel.find({
+            users: {
+                $all: [from, to]
+            }
+        }).sort({updatedAt: 1}).maxTimeMS(30000);
 
-      if (!messages || messages.length === 0) {
-          return res.status(404).json({ message: 'No messages found' });
-      }
+        if (! messages || messages.length === 0) {
+            return res.status(404).json({message: 'No messages found'});
+        }
 
-      const messagesToDisplay = messages.map((msg) => {
-          return {
-              fromSelf: msg.sender.toString() === from,
-              message: msg.message.text
-          };
-      });
+        const messagesToDisplay = messages.map((msg) => {
+            return {
+                fromSelf: msg.sender.toString() === from,
+                message: msg.message.text
+            };
+        });
 
-      return res.status(200).json({ messagesToDisplay, messages });
-  } catch (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Internal server error' });
-  }
+        return res.status(200).json({messagesToDisplay, messages});
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({message: 'Internal server error'});
+    }
 };
